2921b11f330bda59104b594338f01c873190a366
eugenet12 <eugenet@princeton.edu>
2014-03-21 18:33:12 -0400

    add search, song page, login, signup views. Add footer, partial header

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -14,6 +14,26 @@ enchord.config(['$routeProvider', function($routeProvider) {
 		controller: 'AboutController',
 		templateUrl: 'partials/about.html'
 	})
+	.when('/search/:query',
+	{
+		controller: 'SearchController',
+		templateUrl: 'partials/search.html'
+	})
+	.when('/view/:songId',
+	{
+		controller: 'ViewController',
+		templateUrl: 'partials/view_song.html'
+	})
+	.when('/login',
+	{
+		controller: 'LoginController',
+		templateUrl: 'partials/login.html'
+	})
+	.when('/signup',
+	{
+		controller: 'SignupController',
+		templateUrl: 'partials/signup.html'
+	})
 	.otherwise({
 		redirectTo: '/'
 	});
--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -12,3 +12,25 @@ enchordControllers.controller('HomeController', ['$scope',
 enchordControllers.controller('AboutController', ['$scope',
 	function($scope){
 	}]);
+
+// Search page controller
+enchordControllers.controller('SearchController', ['$scope', '$routeParams',
+	function($scope, $routeParams) {
+		$scope.query = $routeParams.query;
+	}]);
+
+// Song page (view) controller
+enchordControllers.controller('ViewController', ['$scope', '$routeParams',
+	function($scope, $routeParams){
+		$scope.song = {songId: $routeParams.songId, title: 'Temp'}
+	}]);
+
+// Signup controller
+enchordControllers.controller('SignupController', ['$scope',
+	function($scope){
+	}]);
+
+// Login controller
+enchordControllers.controller('LoginController', ['$scope',
+	function($scope){
+	}]);
