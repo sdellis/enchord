a5aeabeb02f605dbf07e4a90791cb28aca9cecf6
hseog <seog.heemin@gmail.com>
2014-03-21 20:49:37 -0400

    user registration and validation through mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+var Schema = require('../schemas/user');
 
 /*
  * GET home page.
@@ -6,3 +7,25 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Enchord' });
 };
+
+exports.auth = function(req, res) {
+	if (!req.isAuthenticated()) res.send(401);
+	else next();
+};
+
+exports.signup = function(req, res) {
+	var User = new Schema({
+		user: req.body.username,
+		email: req.body.email,
+		password: req.body.password
+	});
+	User.save(function (err, product, numberAffected) {
+		if (err) {
+			console.log(err);
+			res.status(500).json({status:'failure'});
+			return;
+		}
+		console.log('success!');
+	})
+	res.redirect('#/');
+}
