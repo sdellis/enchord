17f173344a0e94581ffe0a0b9aa33597cc087e3e
Heemin <seog.heemin@gmail.com>
2014-03-28 16:38:58 -0400

    live db

--- models/db.js
+++ models/db.js
@@ -1,5 +1,6 @@
 // Mongoose with mongolab
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://jemah:enchord2016@ds029117.mongolab.com:29117/enchord');
+//mongoose.connect('mongodb://jemah_live:enchord2016@ds041377.mongolab.com:41377/enchord_live');
 
 exports.mongoose = mongoose.connection;
