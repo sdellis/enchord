7cd5d32470e7471c1601d69eb12e53327ab9265a
jc29 <jc29@princeton.edu>
2014-03-31 20:48:54 -0400

    fixed rendering of editSong

--- routes/routes.js
+++ routes/routes.js
@@ -55,7 +55,7 @@ module.exports = function(app, passport, db) {
 		app.post('/createsong', isLoggedIn, utils.newSong);
 		
 		app.get('/editsong', isLoggedIn, function(req, res) {
-			res.render('editsong.ejs', {title: 'enchord', isNew: 'true'});
+			res.render('editsong.ejs', {title: 'enchord', isNew: 'true', user: req.user, message: ''});
 		});
 		
 		app.post('/editsong', isLoggedIn, utils.editSong);
--- routes/utils.js
+++ routes/utils.js
@@ -2,6 +2,7 @@ var songSchema = require('../models/schemas/song');
 
 exports.newSong = function(req, res) {
 	res.send('Hello');
+	
 };
 
 exports.editSong = function(req, res) {
@@ -11,3 +12,15 @@ exports.editSong = function(req, res) {
 exports.deleteSong = function(req, res) {
 	
 };
+
+// get data from form
+	
+	// validate data
+	
+	// create object with data
+	
+	// save data
+	
+	// make sure save is ok
+	
+	// redirect to new page based on results
