69627db2a58eacfd621fd18c3be7b8394c81fa74
eugenet12 <eugenet@princeton.edu>
2014-04-01 17:08:05 -0400

    Updated editsong with views. Refactor create/edit/delete songs

--- routes/utils.js
+++ routes/utils.js
@@ -15,21 +15,24 @@ exports.createSong = function(req, res) {
 		});
 	if (song.title.trim() == '') {
 		console.log('empty title');
-		res.render('editsong.ejs', {title: 'enchord', isNew: true, song: songEmpty, message: 'Empty title'});
+		res.send({song: song, message: 'Error: Empty title', hasError: true, isNew: true});
+		// res.render('editsong.ejs', {title: 'enchord', isNew: true, song: songEmpty, message: 'Empty title'});
 	}
 	if (song.artist.trim() == '') {
 		console.log('empty artist');
-		res.render('editsong.ejs', {title: 'enchord', isNew: true, song: songEmpty, message: 'Empty artist'});
+		res.send({song: song, message: 'Error: Empty artist', hasError: true, isNew: true});
+		// res.render('editsong.ejs', {title: 'enchord', isNew: true, song: songEmpty, message: 'Empty artist'});
 	}
 		
 	song.save(function (err, product, numberAffected) {
 			if (err) {
 				console.log(err);
-				res.status(500).json({status: 'fail'});
+				res.status(500).json({message: 'Internal server error', hasError: true});
 				return;
 			}
 			console.log('success saved');
-			res.render('editsong.ejs', {title: 'enchord', isNew: false, song: product, message: 'successfully saved'});
+			res.send({song: song, message: 'Successfully created', hasError: false, isNew: false});
+			// res.render('editsong.ejs', {title: 'enchord', isNew: false, song: product, message: 'successfully saved'});
 			});
 };
 
@@ -39,7 +42,7 @@ exports.editSong = function(req, res) {
 	var song = songSchema.find({_id: id}, function(err, docs) {
 		if (err) {
 			console.log(err);
-			res.status(500).json({status: 'fail'});
+			res.status(500).json({message: 'Internal server error', hasError: true});
 			return;
 		} 
 	});
@@ -47,11 +50,11 @@ exports.editSong = function(req, res) {
 	songSchema.update(song, {title: req.body.title, artist: req.body.artist, genre: req.body.genre}, function(err, numberAffected, rawResponse) {
 		if (err) {
 			console.log(err);
-			res.status(500).json({status: 'fail'});
+			res.status(500).json({message: 'Internal server error', hasError: true});
 			return;
 		}
 		console.log('success edit');
-		res.redirect('/members');
+		res.send({song: song, message: 'Successfully saved', hasError: false, isNew: false});
 		});
 };
 
@@ -61,11 +64,11 @@ exports.deleteSong = function(req, res) {
 	songSchema.remove({_id: id}, function(err) {
 		if (err) {
 			console.log(err);
-			res.status(500).json({status: 'fail'});
+			res.status(500).json({message: 'Internal server error', hasError: true});
 			return;
 		}
 		console.log('success delete');
-		res.redirect('/members');
+		res.send({message: 'Successfully deleted', hasError: false, isNew: false, isDeleted: true});
 	});
 };
 
