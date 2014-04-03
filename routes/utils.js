c1bf6038487b6f304a551562b39bcd8145f7412c
jc29 <jc29@princeton.edu>
2014-04-02 22:17:32 -0400

    Fixed editSong, createSong

--- routes/utils.js
+++ routes/utils.js
@@ -1,5 +1,6 @@
 //Mongo
 var songSchema = require('../models/schemas/song');
+var ObjectId = require('mongoose/lib/types/objectid');
 
 var songEmpty = {
 		title: '',
@@ -37,63 +38,34 @@ exports.createSong = function(req, res) {
 };
 
 exports.editSong = function(req, res) {
-	var id = req.body._id;
-	console.log(id);
-	return;
-	
-	var song = songSchema.find({_id: id}, function(err, docs) {
+	var id = req.body._id; //USE OID NOT TITLE THIS IS ONLY FOR TESTING
+	var song = new songSchema({
+		title: req.body.title,
+		artist: req.body.artist,
+		genre: req.body.genre,
+		_id: id
+		});
+	songSchema.update({_id: id}, {title: req.body.title, artist: req.body.artist, genre: req.body.genre, }, function(err, numberAffected, rawResponse) {
 		if (err) {
 			console.log(err);
-			res.status(500).json({message: 'Internal server error', hasError: true});
-			return;
-		} 
-	});
-	
-	songSchema.update(song, {title: req.body.title, artist: req.body.artist, genre: req.body.genre}, function(err, numberAffected, rawResponse) {
-		if (err) {
-			console.log(err);
-			res.status(500).json({message: 'Internal server error', hasError: true});
+			res.status(500).json({message: 'Internal server error 2', hasError: true});
 			return;
 		}
 		console.log('success edit');
+		console.log(rawResponse);
 		res.send({song: song, message: 'Successfully saved', hasError: false, isNew: false});
 		});
 };
 
 exports.deleteSong = function(req, res) {
 	var id = req.body._id;
-	
 	songSchema.remove({_id: id}, function(err) {
 		if (err) {
 			console.log(err);
-			res.status(500).json({message: 'Internal server error', hasError: true});
+			res.status(500).json({message: 'Internal server error 3', hasError: true});
 			return;
 		}
 		console.log('success delete');
 		res.send({message: 'Successfully deleted', hasError: false, isNew: false, isDeleted: true});
 	});
 };
-
-//req.body
-// : title. artist genre
-
-
-// get data from form
-	
-// validate data
-	//check not empty (only title and artist required)
-	//done elsewhere
-// create object with data
-
-// save data
-
-// make sure save is ok
-	
-// redirect to new page based on results
-
-
-
-//get Song
-/*
-songSchema.find( {
-*/
