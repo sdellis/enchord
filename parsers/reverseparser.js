098c6f4d426dace329a5df9e289a0310f64803d7
Matthew Wang <mail4matthew@gmail.com>
2014-04-19 11:07:28 -0400

    reverseparser start, commit to fetch

--- parsers/reverseparser.js
+++ parsers/reverseparser.js
@@ -3,12 +3,13 @@
 *Matthew Wang, jemah
 *
 */
-var commonheaders = []
+var commonheaders = new Array( /^\s*(\d(th|nd|rd)?)?\s*verse(s)?([ _]*\d+)?\s*:\s*$/m,/^\s*(\d(th|nd|rd)?)?\s*chorus(es)?([ _]*\d+)?\s*:\s*$/m,/^\s*(\d(th|nd|rd)?)?\s*bridge(s)?([ _]*\d+)?\s*:\s*$/m, /^\s*(\d(th|nd|rd)?)?\s*intro(duction)?([ _]*\d+)?\s*:\s*$/m,/^\s*(\d(th|nd|rd)?)?\s*outro([ _]*\d+)?\s*:\s*$/m,/^\s*(\d(th|nd|rd)?)?\s*instrumental([ _]*\d+)?\s*:\s*$/m,/^\s*(\d(th|nd|rd)?)?\s*end(ing)?([ _]*\d+)?\s*:\s*$/m,/^\s*(\d(th|nd|rd)?)?\s*break(down)([ _]*\d+)?\s*:\s*$/m,/^\s*(\d(th|nd|rd)?)?\s*tag([ _]*\d+)?\s*:\s*$/m)
 
 
 function lineType(line)
 {
 	//is it a header?
-	
-	
+	for(header in commonheaders)
+		if(header.test(line))
+			return "header";
 }
