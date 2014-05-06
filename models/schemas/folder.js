c4c9f717b24e709c13a652ebb03d4fc02536d451
Heemin <seog.heemin@gmail.com>
2014-05-05 23:13:51 -0400

    little fixes (comments, remove extra modules, change pagination view on somewhere, remove more bands stuff)

--- models/schemas/folder.js
+++ models/schemas/folder.js
@@ -3,6 +3,5 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('folder', {
 	name: String,
 	author_name: String, //original creator of the folder(can be changed to array)
-	//shared: [String],
 	author_id: String, //array of strings
 });
