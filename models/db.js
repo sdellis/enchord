c90ff7425e0c74c3c2a4932c2a2b9495dca1b549
eugenet12 <eugenet@princeton.edu>
2014-05-06 23:56:31 -0400

    correct db

--- models/db.js
+++ models/db.js
@@ -2,9 +2,9 @@
 var mongoose = require('mongoose');
 
 /* live db */
-mongoose.connect('mongodb://jemah:enchord2016@ds029117.mongolab.com:29117/enchord');
+// mongoose.connect('mongodb://jemah:enchord2016@ds029117.mongolab.com:29117/enchord');
 
 /* local testing db */
-// mongoose.connect('mongodb://jemah_local:enchord2016@ds041387.mongolab.com:41387/enchord_local');
+mongoose.connect('mongodb://jemah_local:enchord2016@ds041387.mongolab.com:41387/enchord_local');
 
 exports.mongoose = mongoose.connection;
