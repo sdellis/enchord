848453436f314ee1815fb4a1641766fd68c3062a
eugenet12 <eugenet@princeton.edu>
2014-05-07 23:48:26 -0400

    fix transposition

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -14,37 +14,6 @@ var cleanSong = function(song) {
 		song.pub=true;
 	return song;
 }
-var getDate = function() {
-	var today = new Date();
-	var dd = today.getDate();
-	var mm = today.getMonth()+1; //January is 0!
-	var yyyy = today.getFullYear();
-	var h = today.getHours();
-	var m = today.getMinutes();
-	var s = today.getSeconds();
-	var ms = today.getMilliseconds();
-
-	if(dd<10) {
-    	dd='0'+dd
-	} 
-	if(mm<10) {
-    	mm='0'+mm
-	}
-	if(h<10) {
-    	h='0'+h
-	} 
-	if(m<10) {
-    	m='0'+m
-	} 
-	if(s<10) {
-    	s='0'+s
-	} 
-	if(ms<10) {
-    	ms='0'+ms
-	} 
-	today = mm+dd+yyyy+h+m+s+ms;
-	return today;
-}
 
 //for searching. get rid of invalid characters
 function purify(query, possib) {
@@ -316,6 +285,9 @@ enchordControllers.controller('SongViewController', [
 		$scope.isAuthor = false;
 		$scope.font = "Helvetica"
 		$scope.fontsize = "14"
+		$scope.steps = "0";
+		$scope.preference = "♯";
+		$scope.origData = "";
 		$scope.transposed = 0;
 		$scope.hasvoted = function() {
 			$http({
@@ -351,6 +323,7 @@ enchordControllers.controller('SongViewController', [
 				}).success(function(data) {
 					console.log(data);
 					$scope.song = data.song;
+					$scope.origData = $scope.song.data;
 					$scope.parsehtml();
 					if (isLoggedIn) {
 						$scope.hasvoted();
@@ -498,17 +471,35 @@ enchordControllers.controller('SongViewController', [
 		}
 		//transpose
 		$scope.transpose = function() {
+			console.log($scope.origData);
+			var pref = ''
+			if ($scope.preference == '♯') {
+				pref = 's'
+			} else {
+				pref = 'f';
+			}
 			$http({
 				method: 'POST',
-				url: '/view/transpose',
-				//how do i change this to include other stuff?
-				data : $.param({data: $scope.songdata, step: $scope.steps, sf: $scope.sf}),
+				url: '/edit/transpose',
+				data : $.param({data: $scope.origData, step: $scope.steps, sf: pref}),
 				headers : {'Content-Type': 'application/x-www-form-urlencoded' }
 			}).success(function(data) {
-				// is this the html shown?
-				$scope.transposed = $scope.transposed + $scope.steps;
-				$scope.song.result = data;
+				console.log(data);
+				$scope.song.data = data.transposedSong;
+				$scope.parsehtml();
+				$scope.leaveTranspose();
+				// $scope.steps = 0;
+				// $scope.transposed = $scope.transposed + $scope.steps;
+				// $scope.markupForm = data;
 			});
+		}
+
+		$scope.enterTranspose = function() {
+			$scope.transposeMode = true;
+		}
+
+		$scope.leaveTranspose = function() {
+			$scope.transposeMode = false;
 		}
 	}]);
 
@@ -533,6 +524,7 @@ enchordControllers.controller('SongEditController', [
 		$scope.preference = "♯"
 		$scope.reverseParseMode = false;
 		$scope.isSongSaved = false;
+		$scope.transposeMode = false;
   		var win = $window;
   		var unWatch = $scope.$watch('songEditForm.$dirty || markupForm.$dirty', function(value) {
     		if(value) {
@@ -739,6 +731,7 @@ enchordControllers.controller('SongEditController', [
 				$scope.song.data = data.transposedSong;
 				$scope.parsehtml();
 				$scope.steps = 0;
+				$scope.leaveTranspose();
 				// $scope.transposed = $scope.transposed + $scope.steps;
 				// $scope.markupForm = data;
 			});
@@ -750,6 +743,14 @@ enchordControllers.controller('SongEditController', [
 
 		$scope.leaveReverseParseMode = function() {
 			$scope.reverseParseMode = false;
+		}
+
+		$scope.enterTranspose = function() {
+			$scope.transposeMode = true;
+		}
+
+		$scope.leaveTranspose = function() {
+			$scope.transposeMode = false;
 		}
 
 		$scope.reverseParse = function() {
