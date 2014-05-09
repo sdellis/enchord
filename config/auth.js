11655c238ac2997b2940fc251394daf3c01cfd59
eugenet12 <eugenet@princeton.edu>
2014-05-09 00:26:00 -0400

    enchord.me

--- config/auth.js
+++ config/auth.js
@@ -4,19 +4,19 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: '650161668383558', 
 		'clientSecret' 	: '8a71020603f6b6c983af4f1a49607598', 
-		'callbackURL' 	: 'http://enchord.herokuapp.com/auth/facebook/callback'
+		'callbackURL' 	: 'http://www.enchord.me/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
 		'consumerKey' 		: 'taepcFeYyOSDA5T5NMw7qgqnC',
 		'consumerSecret' 	: 'JQJ7odBqjjmXdUs8OItB4Uep1PwyzixNHrsI11lLYG4b0gVpdV',
-		'callbackURL' 		: 'http://enchord.herokuapp.com/auth/twitter/callback'
+		'callbackURL' 		: 'http://www.enchord.me/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
 		'clientID' 		: '1082883114760-8rb8bv5tc4m13a0p0rh4g26t3e91gj9o.apps.googleusercontent.com',
 		'clientSecret' 	: 'u4YKYAyWueWB9WDry3oRgdvC',
-		'callbackURL' 	: 'http://enchord.herokuapp.com/auth/google/callback'
+		'callbackURL' 	: 'http://www.enchord.me/auth/google/callback'
 	}
 
 };
